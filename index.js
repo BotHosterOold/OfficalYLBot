c12a2c074c6f469eca04b5b38a7e48dc3093c5e2
BotHosterOold <50768383+BotHosterOold@users.noreply.github.com>
2019-05-19 09:12:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ bot.on("message", (message) => {
 }
 
 if (message.content.toLowerCase().startsWith(prefix + `new`)) {
-    msg.delete();
     const reason = message.content.split(" ").slice(1).join(" ");
     if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
     if (message.guild.channels.exists("name", "TicketHelp")) return message.channel.send(`你現在已經有一個Support ticket了.`);
@@ -77,6 +76,7 @@ if (message.content.toLowerCase().startsWith(prefix + `new`)) {
         c.overwritePermissions(message.author, {
             SEND_MESSAGES: true,
             READ_MESSAGES: true
+	m1.delete();
         });
         message.channel.send(`:white_check_mark: 以創建您的私人頻道, #${c.name}.`);
         const embed = new Discord.RichEmbed()
